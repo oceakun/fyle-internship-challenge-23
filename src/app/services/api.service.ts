2d71f3a2745be7fb7688f3276151aae728f5ec70
oceakun <sagardeep1811@hotmail.com>
2023-11-07 18:57:09 +0530

    feat: create service for Github API

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,19 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(
-    private httpClient: HttpClient
-  ) { }
-
-  getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
+  getUser(githubUsername: string): Observable<any> {
+    return this.httpClient.get(
+      `https://api.github.com/users/${githubUsername}`
+    );
   }
 
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  getRepos(githubUsername: string, page: number): Observable<any[]> {
+    console.log('page', page);
+    return this.httpClient
+      .get<any[]>(`https://api.github.com/users/${githubUsername}/repos?page=${page}&per_page=10`)
+      .pipe(
+        catchError((error: any) => {
+          console.error('Error fetching repositories:', error);
+          return throwError(()=> new Error(
+            'Something went wrong while fetching repositories. Please try again later.')
+          );
+        })
+      );
+  }
 }
