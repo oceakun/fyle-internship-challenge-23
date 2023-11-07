17a95d63a2bd333762d3291dc0eff88b5189180a
oceakun <sagardeep1811@hotmail.com>
2023-11-07 19:01:15 +0530

    feat: integrate components and implement data management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
+  @Input() user: any;
+  @Input() repos: any;
+  @Input() msgVal: string = 'Please enter a username to get started !';
+  @Input() pageNumber: number = 1;
+  usernameEntered: any;
+  showLoader: boolean = false;
+  showBioLoader: boolean = false;
+  showReposLoader: boolean = false;
+  showContent: boolean = false;
+  showMsg: boolean = true;
+
+  onPageNumberChange(event: number) {
+    const newValue = event;
+    this.pageNumber = newValue;
+    this.getRepoData(this.usernameEntered.username, this.pageNumber);
+  }
+
+  usernameForm = this.formBuilder.group({
+    username: '',
+  });
+
   constructor(
+    private formBuilder: FormBuilder,
     private apiService: ApiService
   ) {}
 
-  ngOnInit() {
-    this.apiService.getUser('johnpapa').subscribe(console.log);
+  onSubmit(): void {
+    this.usernameEntered = this.usernameForm.value;
+    this.usernameForm.reset();
+    this.showMsg = false;
+    this.showBioLoader = true;
+    this.showReposLoader = true;
+    this.showContent = false;
+    this.getUserData(this.usernameEntered.username);
+    this.getRepoData(this.usernameEntered.username, this.pageNumber);
+  }
+
+  getRepoData(username: string, pageNumber: number): void {
+
+    this.apiService.getRepos(username, pageNumber).subscribe({
+      next: (reposData: any) => {
+        this.repos = reposData;
+        this.showReposLoader = false;
+      },
+      error: (error) => {
+        console.error('Error fetching repos data:', error);
+        this.msgVal ='Error!! Please check the validity of username entered or the status internet connectivity.';
+        this.showMsg = true;
+        this.showBioLoader = false;
+        this.showReposLoader = false;
+        this.showContent = false;
+      },
+    });
+  }
+
+  getUserData(username: string): void {
+    this.showReposLoader = true;
+
+    this.apiService.getUser(username).subscribe({
+      next: (userData: any) => {
+        this.user = userData;
+        this.showBioLoader = false;
+        this.showContent = true;
+      },
+      error: (error) => {
+        console.error('Error fetching user data:', error);
+        this.msgVal ='Error!! Please check the validity of username entered or the status internet connectivity.';
+
+        this.showMsg = true;
+        this.showBioLoader = false;
+        this.showReposLoader = false;
+        this.showContent = false;
+      },
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { ApiService } from './services/api.service';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from  '@angular/common/http';
+import { BioComponent } from './components/bio/bio.component';
+import { MainComponent } from './components/main/main.component';
+import { PaginatorComponent } from './components/paginator/paginator.component';
+import { ReposComponent } from './components/repos/repos.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { BioLoaderComponent } from './components/bio-loader/bio-loader.component';
+import { ReposLoaderComponent } from './components/repos-loader/repos-loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BioComponent,
+    MainComponent,
+    PaginatorComponent,
+    ReposComponent,
+    MessagesComponent,
+    BioLoaderComponent,
+    ReposLoaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, HttpClientModule, FormsModule, ReactiveFormsModule , NgxSkeletonLoaderModule,],
+  providers: [ApiService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
